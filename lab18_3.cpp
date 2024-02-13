9ee84a39aa7914bb81473dc7ac742fa47f0226b4
Salisatoru <95071118+Salisatoru@users.noreply.github.com>
2024-02-13 16:20:05 +0700

    finish lab18_3.cpp

--- lab18_3.cpp
+++ lab18_3.cpp
@@ -8,9 +8,10 @@
 using namespace std;
 
 struct student{
-
-    //Define struct student with four member (name ,id , gender, gpa);
-    
+	string name;
+	int id;
+	char gender;
+	float gpa;
 };
 
 struct course{
@@ -20,9 +21,9 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){ //There is something wrong in this line.
 	for(unsigned int i = 0; i < allstudents.size(); i++){
-		if(allstudents[i].id  == key) return &allstudents[i];
+		if(allstudents[i].id == key) return &allstudents[i];
 	}
 	return 0;
 }
@@ -37,18 +38,18 @@ void printreport(vector<course> allcourses){
 		for(unsigned int j = 0; j < allcourses[i].lecture_list.size();j++){
 			if(j != 0) cout << ", ";
 			cout << allcourses[i].lecture_list[j];
-		}
-		cout << "\n\n";
-		cout << left << setw(20);
-		cout << "Students:";
+	}
+	cout << "\n\n";
+	cout << left << setw(20);
+	cout << "Students:";
 		for(unsigned int j = 0; j < allcourses[i].student_list.size();j++){
 			if(j != 0) cout << left << setw(20) << " ";
 			cout << left << setw(20) << allcourses[i].student_list[j]->name;
 			cout << left << setw(10) << allcourses[i].student_list[j]->id;
 			cout << left << setw(3) << allcourses[i].student_list[j]->gender;
 			cout << left << setw(5) << setprecision(2) << fixed << allcourses[i].student_list[j]->gpa << "\n";
-		} 
-		
+		}
+
 	}
 	cout << "-----------------------------------------------------------------------------\n";
 }
@@ -58,17 +59,18 @@ int main(){
 	ifstream course_file("courses.txt");
 	vector<student> allstudents;
 	vector<course> allcourses;
-	
-	string textline;
-	
-	while(getline(student_file,textline)){
-		student s; 
-		
-		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
 
-		allstudents.push_back(s); 		
+	string textline;
+
+	while(getline(student_file,textline)){
+		student s;
+		char a[100];
+		sscanf(textline.c_str(), "%[^,],%d,%c,%f", a, &(s.id), &(s.gender), &(s.gpa));
+		s.name = a;
+
+		allstudents.push_back(s);
 	}
-	
+
 	int state = 1;
 	while(getline(course_file,textline)){
 		if(state == 1){
@@ -78,26 +80,30 @@ int main(){
 			c.id = atof(textline.substr(loc+1,5).c_str());
 			getline(course_file,textline);
 			allcourses.push_back(c);
-			state = 2;			
-		}else if(state == 2){
-			if(textline == "> Students"){
-				state = 3;
-			}else{
-			
-			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
-			    
-			}			
+			state = 2;
+	}else if(state == 2){
+		if(textline == "> Students"){
+			state = 3;
 		}else{
-			if(textline == "---------------------------------------"){
-				state = 1;
-			}else{
-				student *p = findstudent(allstudents,atof(textline.c_str()));
-				
-				//Append (push_back) p to student_list of the recently added course in allcourses[];
-				
+
+			//Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
+			if (allcourses.at(0).lecture_list.size() < 4) allcourses.at(0).lecture_list.push_back(textline);
+			else if (allcourses.at(1).lecture_list.size() < 1) allcourses.at(1).lecture_list.push_back(textline);
+			else allcourses.at(2).lecture_list.push_back(textline);
+	}
+	}else{
+		if(textline == "---------------------------------------"){
+			state = 1;
+		}else{
+			student *p = findstudent(allstudents,atof(textline.c_str()));
+
+			//Append (push_back) p to student_list of the recently added course in allcourses[];
+			if (allcourses[0].student_list.size() < 10) allcourses[0].student_list.push_back(p);
+			else if (allcourses[1].student_list.size() < 9) allcourses[1].student_list.push_back(p);
+			else allcourses[2].student_list.push_back(p);
+
 			}
 		}
 	}
 	printreport(allcourses);
-	
 }
